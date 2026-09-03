295b8b0a96fc7d85594a1dcbe34dbcd5ef996203
Johan List <johan.list@di.huc.knaw.nl>
2026-09-03 11:07:09 +0200

    Comment to English

--- cron/entrypoint_cron.sh
+++ cron/entrypoint_cron.sh
@@ -5,7 +5,7 @@ mv /app/crontab /etc/cron.d/crontab
 crontab /etc/cron.d/crontab
 touch /var/log/cron.log
 
-# Eerste keer meteen draaien, anders is het volume leeg tot het volgende hele uur
+# Run right away first time, otherwise the volume is empty until the next whole hour
 /app/crontask.sh >> /var/log/cron.log 2>&1
 
 cron && tail -f /var/log/cron.log
